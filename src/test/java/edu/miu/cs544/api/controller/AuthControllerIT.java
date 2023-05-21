d0a61ae58a1e98fa0ab4101cea70bb8109e8d421
Mesi <yihunm@gmail.com>
2023-05-21 10:30:16 -0500

    initial commit

---
+++ src/test/java/edu/miu/cs544/api/controller/AuthControllerIT.java
@@ -0,0 +1,22 @@
+package edu.miu.cs544.api.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthControllerIT {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefault() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().is4xxClientError());
+    }
+}
---
+++ src/test/java/edu/miu/cs544/api/controller/AuthControllerTest.java
@@ -0,0 +1,17 @@
+package edu.miu.cs544.api.controller;
+
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//@SpringBootTest
+//@AutoConfigureMockMvc
+public class AuthControllerTest {
+    //@Autowired
+    private MockMvc mockMvc;
+
+    //@Test
+    public void shouldReturnDefault() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().is4xxClientError());
+    }
+}
