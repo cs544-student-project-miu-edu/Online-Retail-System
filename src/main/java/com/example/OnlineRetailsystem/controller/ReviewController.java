241468c6427ac2e2c4c035d7286f90eb5914c61a
Rahel Teklu <113157@cs.mum.edu>
2023-05-24 19:54:59 -0500

    saved

--- src/main/java/com/example/OnlineRetailsystem/controller/ReviewController.java
+++ src/main/java/com/example/OnlineRetailsystem/controller/ReviewController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/reviews")
+@RequestMapping("/api/v1/items/{itemID}/reviews")
 public class ReviewController {
     @Autowired
     ReviewService reviewService;
@@ -18,22 +18,22 @@ public class ReviewController {
         return reviewService.getAllReviews();
     }
     @GetMapping("/{id}")
-    public void getReviewById(@PathVariable Long id) {
+    public void getReviewById(@PathVariable int itemID, @PathVariable Long id) {
         reviewService.getReviewById(id);
     }
 
     @PostMapping("/newReview")
-    public Review createNewReview(@RequestBody Review review) {
+    public Review createNewReview(@PathVariable int itemID, @RequestBody Review review) {
         return reviewService.createReview(review);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteReview(@PathVariable Long id) {
+    public void deleteReview(@PathVariable int itemID, @PathVariable Long id) {
         reviewService.deleteReviewById(id);
     }
 
     @PutMapping("/{id}")
-    public Review updateReview(@PathVariable Long id, @RequestBody Review review) {
+    public Review updateReview(@PathVariable int itemID, @PathVariable Long id, @RequestBody Review review) {
         return reviewService.updateReviewById(id, review);
     }
 }
--- src/main/java/com/example/OnlineRetailsystem/service/ReviewService.java
+++ src/main/java/com/example/OnlineRetailsystem/service/ReviewService.java
@@ -6,11 +6,12 @@ import com.example.OnlineRetailsystem.domain.Review;
 import java.util.List;
 
 public interface ReviewService {
+
     public Review createReview(Review review);
+    public List<Review> getAllReviews();
+    public Review getReviewById(Long reviewId);
+    public Review updateReviewById(Long reviewId, Review review);
     public void deleteReviewById(Long reviewID);
 
-    public Review updateReviewById(Long reviewId, Review review);
-    public Review getReviewById(Long reviewId);
-    public List<Review> getAllReviews();
 
 }
